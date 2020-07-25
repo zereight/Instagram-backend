ba80b39793e48697d9b843872a1a0c3105288ac1
zereight <aldwk68@naver.com>
2020-07-26 01:22:33 +0900

    graphql server setting

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("Hi");
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config( { path: path.resolve()+"/.env" });
+
+import { GraphQLServer } from "graphql-yoga";
+
+const PORT = process.env.PORT;
+
+const typeDefs = `
+    type Query{
+        hello: String!
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello: ()=>"Hi!!"
+    }
+};
+
+const server = new GraphQLServer({typeDefs, resolvers});
+server.start( PORT, ()=>{console.log(`Server Started on ${PORT}`)} )
